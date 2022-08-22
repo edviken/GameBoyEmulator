6a0af868470f1fab626e95b96b51de5e5955ce92
Martin <martin_edviken@hotmail.com>
2022-08-22 20:44:13 +0200

    Solve filepath issues

--- test/CartridgeInterpreterTest.cpp
+++ test/CartridgeInterpreterTest.cpp
@@ -2,6 +2,7 @@
 // Created by Martin Edviken on 2022-08-20.
 //
 #include "disassembler/Cartridge.hpp"
+#include "root_path.h"
 
 #include <gtest/gtest.h>
 
@@ -17,7 +18,9 @@ class CartridgeInterpreterTest : public ::testing::Test {
 TEST_F(CartridgeInterpreterTest, readBinaryData) {
   std::vector<uint8_t> refData{1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
   std::vector<uint8_t> data;
-  cartridge.readDataFromFile("../../test/cartridges/simpleInputData.bin");
+  std::string root_path = PROJECT_ROOT_STRING;
+  std::string file_path = root_path + "/test/cartridges/simpleInputData.bin";
+  cartridge.readDataFromFile(file_path);
   data = cartridge.getData();
   ASSERT_EQ(data, refData);
 }
